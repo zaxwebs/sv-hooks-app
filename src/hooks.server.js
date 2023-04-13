7952044f8fc34368f51373fbe9243155095dcbed
zaxwebs <zifidigital@gmail.com>
2023-04-13 16:23:45 +0530

    initial commit

---
+++ src/hooks.server.js
@@ -0,0 +1,8 @@
+export async function handle({ event, resolve }) {
+	if (event.url.pathname.startsWith('/custom')) {
+		return new Response('custom response');
+	}
+
+	const response = await resolve(event);
+	return response;
+}
