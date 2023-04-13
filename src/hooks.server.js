496fe5e19442de74f3d644a1d8e95dab74088d74
zaxwebs <zifidigital@gmail.com>
2023-04-13 17:40:06 +0530

    added event logging

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -10,4 +10,26 @@ const handleCustomRoute = async ({ event, resolve }) => {
 	return response;
 }
 
-export const handle = sequence(handleCustomRoute);
+const handleConsoleRoute = async ({ event, resolve }) => {
+
+	if (event.url.pathname.startsWith('/console')) {
+		console.log('Hello world!');
+		return new Response('There\'s stuff in your server console.');
+	}
+
+	const response = await resolve(event);
+	return response;
+}
+
+const handleLogEvent = async (input) => {
+
+	if (input.event.url.pathname.startsWith('/event')) {
+		console.log(input);
+		return new Response('Event has been logged.');
+	}
+
+	const response = await resolve(input);
+	return response;
+}
+
+export const handle = sequence(handleCustomRoute, handleConsoleRoute, handleLogEvent);
